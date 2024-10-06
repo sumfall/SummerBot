5ecd7bf5904f335485abfeaf03bd8f0c68c8475e
1-sunrise <plb32007@gmail.com>
2024-10-06 19:05:13 -0400

    v1

---
+++ main.py
@@ -0,0 +1,44 @@
+import interactions
+from interactions import (
+    Client,
+    Intents,
+    slash_command,
+    SlashContext,
+    OptionType,
+    slash_option,
+    check,
+    is_owner,
+    integration_types,
+)
+
+BOT_TOKEN = open("token.txt", "r").read().strip()
+
+bot = Client(intents=Intents.ALL, basic_logging=True)
+
+
+@interactions.listen()
+async def on_startup():
+    print("SummerBot is ready")
+
+
+# Slash commands
+@slash_command(name="say", description="Makes the bot say what you want it to say:")
+@integration_types(user=True)
+@check(is_owner())
+@slash_option(
+    name="text",
+    description="Adds text",
+    required=False,
+    opt_type=OptionType.STRING,
+)
+async def channel_function(ctx: SlashContext, text: str = None):
+    if not text:
+        print("ERROR: Please select an option")
+        await ctx.send("ERROR: Please select an option", ephemeral=True)
+    elif text:
+        print("Sent: " + text)
+        await ctx.send(text, ephemeral=False)
+
+
+if __name__ == "__main__":
+    bot.start(BOT_TOKEN)
